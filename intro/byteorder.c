16a1f7d5e09f7c7b143d47831a534ae461506c18
ryuever <liuyouchao111@gmail.com>
2014-02-23 16:12:15 +0900

    program to determin host byte order little/big endian

--- intro/byteorder.c
+++ intro/byteorder.c
@@ -1,4 +1,4 @@
-#include	"unp.h"
+#include <stdio.h>
 
 int
 main(int argc, char **argv)
@@ -9,7 +9,8 @@ main(int argc, char **argv)
     } un;
 
 	un.s = 0x0102;
-	printf("%s: ", CPU_VENDOR_OS);
+    printf("un.s is %d.\n", un.s);
+	/* printf("%s: ", CPU_VENDOR_OS); */
 	if (sizeof(short) == 2) {
 		if (un.c[0] == 1 && un.c[1] == 2)
 			printf("big-endian\n");
@@ -18,7 +19,8 @@ main(int argc, char **argv)
 		else
 			printf("unknown\n");
 	} else
-		printf("sizeof(short) = %d\n", sizeof(short));
+      /* printf("sizeof(short) = %d\n", sizeof(short)); */
+      printf("sizeof(short) = %zu\n", sizeof(short));
 
-	exit(0);
+	return 0;
 }
