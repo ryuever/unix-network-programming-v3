9a770c390ba0d56c839c8c9349d5fda6e027de66
ryuever <liuyouchao111@gmail.com>
2014-03-10 17:21:47 +0900

    add a comment

--- tcpcliserv/sigchldwaitpid.c
+++ tcpcliserv/sigchldwaitpid.c
@@ -5,7 +5,8 @@ sig_chld(int signo){
   pid_t	pid;
   int		stat;
 
-    
+  // constant WNOHANG tells waitpid not to block if there are running children that 
+  // have not yet terminated.
   while ( (pid = waitpid(-1, &stat, WNOHANG)) > 0)
     printf("child %d terminated\n", pid);
   return;
